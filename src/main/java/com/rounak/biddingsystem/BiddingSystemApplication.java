e269544f94e316fe1298b71d13125de31a2e332e
rounakr <roraj@castlighthealth.com>
2020-10-10 12:47:22 +0530

    code skeleton - initial commit

---
+++ src/main/java/com/rounak/biddingsystem/BiddingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rounak.biddingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiddingSystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BiddingSystemApplication.class, args);
+  }
+
+}
