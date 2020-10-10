cd0b6da96fde0454238e5e3b3a6d09efe76c7a86
rounakr <roraj@castlighthealth.com>
2020-10-10 13:37:55 +0530

    add Constants

---
+++ src/main/java/com/rounak/biddingsystem/Constants.java
@@ -0,0 +1,8 @@
+package com.rounak.biddingsystem;
+
+public class Constants {
+
+  public static final String AUCTION_BASE_URI = "/auction";
+  public static final String PLACE_BID_URI = "/{itemCode}/bid";
+
+}
