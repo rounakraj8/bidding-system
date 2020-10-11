4b53fdd173417903a9e41eb76ede7dba117a7848
rounakr <roraj@castlighthealth.com>
2020-10-11 12:07:37 +0530

    rename file

---
+++ src/main/java/com/rounak/biddingsystem/constants/BiddingSystemConstants.java
@@ -0,0 +1,8 @@
+package com.rounak.biddingsystem.constants;
+
+public class BiddingSystemConstants {
+
+  public static final String AUCTION_BASE_URI = "/auction";
+  public static final String AUCTION_PLACE_BID_URI = "/{itemCode}/bid";
+
+}
