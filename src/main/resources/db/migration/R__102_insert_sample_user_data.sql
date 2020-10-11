bc08c6e7004117df2a97cdfb6d5abf6e64384599
rounakr <roraj@castlighthealth.com>
2020-10-11 13:59:14 +0530

    update user sample data

--- src/main/resources/db/migration/R__102_insert_sample_user_data.sql
+++ src/main/resources/db/migration/R__102_insert_sample_user_data.sql
@@ -1,4 +1,4 @@
 INSERT INTO `user`
-VALUES (1, 'USER_1'),
-       (2, 'USER_2'),
-       (3, 'USER_3');
+VALUES (1, 'USER_1', 'LOGGED_IN'),
+       (2, 'USER_2', 'LOGGED_OUT'),
+       (3, 'USER_3', 'LOGGED_IN');
