a5f3f9410b16bf606f2019a270be1bca56fc278e
rounakr <roraj@castlighthealth.com>
2020-10-11 14:02:42 +0530

    accept userId from requestHeader for checking if user is loggedIn

--- src/main/java/com/rounak/biddingsystem/controller/AuctionController.java
+++ src/main/java/com/rounak/biddingsystem/controller/AuctionController.java
@@ -1,7 +1,7 @@
 package com.rounak.biddingsystem.controller;
 
 import static com.rounak.biddingsystem.constants.BiddingSystemConstants.AUCTION_BASE_URI;
-import static com.rounak.biddingsystem.constants.BiddingSystemConstants.AUCTION_PLACE_BID_URI;
+import static com.rounak.biddingsystem.constants.BiddingSystemConstants.AUCTION_BID_URI;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import com.rounak.biddingsystem.dto.AuctionDto;
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -40,11 +41,12 @@ public class AuctionController {
     return auctionService.getAuctionByStatus(auctionStatus);
   }
 
-  @PostMapping(value = AUCTION_PLACE_BID_URI)
+  @PostMapping(value = AUCTION_BID_URI)
   @ResponseStatus(HttpStatus.CREATED)
-  public void placeBid(@PathVariable String itemCode, @RequestBody Double bidAmount) {
+  public void placeBid(@PathVariable String itemCode, @RequestBody Double bidAmount,
+      @RequestHeader Long userId) {
     log.info("Request received for placing bid for item code {}, bid amount is {}", itemCode,
         bidAmount);
-    auctionService.placeBid(itemCode, bidAmount);
+    auctionService.placeBid(itemCode, bidAmount, userId);
   }
 }
