95019736b650164a9bdbb479380bf30154960a79
rounakr <roraj@castlighthealth.com>
2020-10-11 12:02:03 +0530

    insert sample data in tables

---
+++ src/main/resources/db/migration/R__102_insert_sample_user_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `user`
+VALUES (1, 'USER_1'),
+       (2, 'USER_2'),
+       (3, 'USER_3');
---
+++ src/main/resources/db/migration/R__103_insert_sample_bid_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `bid` (`auction_id`, `user_id`, `bid_amount`, `status`)
+VALUES (123, 1, 1000, 'ACCEPTED'),
+       (123, 2, 1050, 'ACCEPTED'),
+       (123, 3, 1100, 'ACCEPTED');
