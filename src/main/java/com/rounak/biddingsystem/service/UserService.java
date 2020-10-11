0300b9344c5e03005f46d7235f9a8914c08abd78
rounakr <roraj@castlighthealth.com>
2020-10-11 23:34:05 +0530

    refactor

--- src/main/java/com/rounak/biddingsystem/service/UserService.java
+++ src/main/java/com/rounak/biddingsystem/service/UserService.java
@@ -5,4 +5,6 @@ import com.rounak.biddingsystem.entity.User;
 public interface UserService {
 
   User findById(Long userId);
+
+  User checkIfUserIsLoggedIn(Long userId);
 }
--- src/main/java/com/rounak/biddingsystem/service/impl/UserServiceImpl.java
+++ src/main/java/com/rounak/biddingsystem/service/impl/UserServiceImpl.java
@@ -2,7 +2,9 @@ package com.rounak.biddingsystem.service.impl;
 
 import com.rounak.biddingsystem.dao.UserDao;
 import com.rounak.biddingsystem.entity.User;
+import com.rounak.biddingsystem.enums.UserStatus;
 import com.rounak.biddingsystem.exception.UserNotFoundException;
+import com.rounak.biddingsystem.exception.UserNotLoggedInException;
 import com.rounak.biddingsystem.service.UserService;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -22,5 +24,14 @@ public class UserServiceImpl implements UserService {
     } else {
       throw new UserNotFoundException(userId);
     }
+  }
+
+  @Override
+  public User checkIfUserIsLoggedIn(Long userId) {
+    User user = findById(userId);
+    if (UserStatus.getUserStatus(user.getStatus()) == UserStatus.LOGGED_OUT) {
+      throw new UserNotLoggedInException();
+    }
+    return user;
   }
 }
