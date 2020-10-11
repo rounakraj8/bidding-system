25a1faba0e46c17e4c4c4bf3a6935acf3b7479d4
rounakr <roraj@castlighthealth.com>
2020-10-11 23:38:07 +0530

    refactor

--- src/main/java/com/rounak/biddingsystem/service/impl/AuctionServiceImpl.java
+++ src/main/java/com/rounak/biddingsystem/service/impl/AuctionServiceImpl.java
@@ -9,14 +9,11 @@ import com.rounak.biddingsystem.entity.Bid;
 import com.rounak.biddingsystem.entity.User;
 import com.rounak.biddingsystem.enums.AuctionStatus;
 import com.rounak.biddingsystem.enums.BidStatus;
-import com.rounak.biddingsystem.enums.UserStatus;
 import com.rounak.biddingsystem.exception.AuctionNotFoundException;
 import com.rounak.biddingsystem.exception.BidUnacceptableException;
-import com.rounak.biddingsystem.exception.UserNotLoggedInException;
 import com.rounak.biddingsystem.mapper.AuctionMapper;
 import com.rounak.biddingsystem.service.AuctionService;
 import com.rounak.biddingsystem.service.UserService;
-import com.rounak.biddingsystem.util.AuctionUtil;
 import java.util.List;
 import java.util.Optional;
 import javax.transaction.Transactional;
@@ -35,7 +32,9 @@ public class AuctionServiceImpl implements AuctionService {
   private final BidDao bidDao;
   private final UserService userService;
 
+
   @Override
+  @Transactional
   public List<AuctionDto> getAuctionByStatus(String auctionStatus) {
     List<Auction> auctionEntities = auctionDao
         .findByStatus(AuctionStatus.getAuctionStatus(auctionStatus));
@@ -44,17 +43,9 @@ public class AuctionServiceImpl implements AuctionService {
 
   @Override
   public void placeBid(String itemCode, Double bidAmount, Long userId) {
-    User user = checkIfUserIsLoggedIn(userId);
+    User user = userService.checkIfUserIsLoggedIn(userId);
     BidStatus bidStatus = findAndSaveBid(itemCode, bidAmount, user);
-    throwExceptionWhenBidIsNotCreated(itemCode, bidAmount, bidStatus);
-  }
-
-  private User checkIfUserIsLoggedIn(Long userId) {
-    User user = userService.findById(userId);
-    if (UserStatus.getUserStatus(user.getStatus()) == UserStatus.LOGGED_OUT) {
-      throw new UserNotLoggedInException();
-    }
-    return user;
+    handleCasesWhenBidIsNotAcceptable(itemCode, bidAmount, bidStatus);
   }
 
   @Transactional
@@ -70,22 +61,26 @@ public class AuctionServiceImpl implements AuctionService {
 
   @NonNull
   private BidStatus saveIfAuctionIsRunning(Double bidAmount, Auction activeAuction, User user) {
-    Double highestBid = AuctionUtil.getBidAmount(activeAuction);
     BidStatus bidStatus;
-    if (isBidAcceptable(bidAmount, activeAuction.getMinBasePrice(), highestBid,
-        activeAuction.getStepPrice())) {
+    if (isBidAcceptable(bidAmount, activeAuction)) {
       bidStatus = BidStatus.ACCEPTED;
+      saveBid(bidAmount, activeAuction, bidStatus, user);
+      updateAuctionWithBidAmount(bidAmount, activeAuction);
     } else {
       bidStatus = BidStatus.REJECTED;
+      saveBid(bidAmount, activeAuction, bidStatus, user);
     }
-    saveBid(bidAmount, activeAuction, bidStatus, user);
     return bidStatus;
   }
 
-  private boolean isBidAcceptable(Double bidAmount, Double minBasePrice, Double highestBid,
-      Double stepPrice) {
-    return minBasePrice < bidAmount
-        && highestBid + stepPrice <= bidAmount;
+  private void updateAuctionWithBidAmount(Double bidAmount, Auction activeAuction) {
+    activeAuction.setMaxBidAmount(bidAmount);
+    auctionDao.save(activeAuction);
+  }
+
+  private boolean isBidAcceptable(Double bidAmount, Auction auction) {
+    return auction.getMinBasePrice() < bidAmount
+        && auction.getMaxBidAmount() + auction.getStepPrice() <= bidAmount;
   }
 
   private void saveBid(Double bidAmount, Auction activeAuction, BidStatus bidStatus, User user) {
@@ -97,7 +92,7 @@ public class AuctionServiceImpl implements AuctionService {
     bidDao.save(newBid);
   }
 
-  private void throwExceptionWhenBidIsNotCreated(String itemCode, Double bidAmount,
+  private void handleCasesWhenBidIsNotAcceptable(String itemCode, Double bidAmount,
       BidStatus bidStatus) {
     switch (bidStatus) {
       case AUCTION_NOT_FOUND:
